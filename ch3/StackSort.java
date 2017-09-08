8e4548cca4d6c6ae9867d4116e5061ad8de6b0cd
venkateshmantha <manthavenkatesh@gmail.com>
2017-09-07 23:53:10 -0400

    day73

---
+++ ch3/StackSort.java
@@ -0,0 +1,21 @@
+public class StackSort{
+	
+
+	public void sort(Stack<Integer> s){
+
+		Stack<Integer> r = new Stack();
+		while(!s.isEmpty()){
+			int tmp = s.pop();
+			while(!r.isEmpty() && r.peek()>tmp){
+				s.push(r.pop());
+			}
+			r.push(tmp);
+		}
+
+		while(!r.isEmpty()){
+			s.push(r.pop());
+		}
+
+	}
+
+}
